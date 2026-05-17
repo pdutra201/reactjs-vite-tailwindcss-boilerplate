c3df304db41ce1929698eb617bfa80a5c904eb57
Philip Dutra <monsterphilip10@gmail.com>
2026-05-17 12:09:29 -0400

    update readme

--- src/components/sections/Expereince.tsx
+++ src/components/sections/Expereince.tsx
@@ -4,7 +4,7 @@ const Experience: React.FC = () => {
   return (
     <section id="experience" className="bg-yellow-400 flex flex-col-reverse xl:flex-row justify-between items-center py-32 px-12 -mx-[16.7%] relative border-t-8 border-black">
       <div>
-        <h1 className="absolute md:w-64 md:h-20 bg-yellow-400 border-x-8 border-t-8 border-black rounded-t-2xl z-40 xl:-top-[5rem] md:-top-[80px] -top-[52px]  left-2 md:left-8 font-DelaGothicOne md:text-4xl text-xl flex justify-evenly items-center py-2">Experience</h1>
+        <h1 className="absolute md:w-[350px] md:h-20 bg-yellow-400 border-x-8 border-t-8 border-black rounded-t-2xl z-40 xl:-top-[5rem] md:-top-[80px] -top-[52px]  left-2 md:left-8 font-DelaGothicOne md:text-4xl text-xl flex justify-evenly items-center py-2">Experience</h1>
       </div>
     </section>
   )
